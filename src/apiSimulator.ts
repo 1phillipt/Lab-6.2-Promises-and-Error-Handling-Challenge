26230fcc3f3ad8138c007a19a71693aa4b505e32
Phillip Thapa <1thapat@gmail.com>
2026-02-03 11:09:28 -0500

    established fetch product review function and added type product review and salesReport

--- src/apiSimulator.ts
+++ src/apiSimulator.ts
@@ -1,9 +1,21 @@
+export type Product = {
+  id: number;
+  name: string;
+  price: number;
+};
 
 type Review = {
   reviewer: string;
   rating: number;
   comment: string;
 };
+
+export type SalesReport = {
+  totalSales: number;
+  unitsSold: number;
+  averagePrice: number;
+};
+
 const fetchProductCatalog = (): Promise<{ id: number; name: string; price: number }[]
 > => {
   return new Promise((resolve, reject) => {
@@ -22,6 +34,20 @@ const fetchProductCatalog = (): Promise<{ id: number; name: string; price: numbe
 
 console.log(fetchProductCatalog)
 
-const fetchProductReviews =(productId:number):Promise<review[]>=>{
+const fetchProductReviews = (productId:number): Promise<Review[]> =>{
 
-}
+ return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Math.random() < 0.75) {
+        resolve([
+          { reviewer: "Alice", rating: 5, comment: "Great product!" },
+          { reviewer: "Bob", rating: 4, comment: "Worth the price." },
+        ]);
+      } else {
+        reject(`Failed to fetch reviews for product ID ${productId}`)
+        
+      }
+    }, 1500);
+  });
+};
+
