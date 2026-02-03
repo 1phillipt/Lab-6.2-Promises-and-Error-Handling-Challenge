b09a45dabded422f8088c0c4ff7bb44c8a3e048e
Phillip Thapa <1thapat@gmail.com>
2026-02-03 12:45:16 -0500

    adjusted index ts file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
 import { fetchProductCatalog, fetchProductReviews, fetchSalesReport } from "./apiSimulator.js";
+
+
+
+const fetchAllData = async (): Promise<void> => {
+  try {
+    
+    const products = await fetchProductCatalog();
+    console.log("Products:");
+    console.log(products);
+
+    for (const product of products) {
+      const reviews = await fetchProductReviews(product.id);
+      console.log(`Reviews for ${product.name}:`);
+      console.log(reviews);
+    }
+
+    const report = await fetchSalesReport();
+    console.log("Sales Report:");
+    console.log(report);
+
+  } catch (error) {
+        console.log("Error fecthing data", error);
+     
+  } finally {
+    console.log("all api calls have been attempted.");
+  }
+};
+
+fetchAllData();
